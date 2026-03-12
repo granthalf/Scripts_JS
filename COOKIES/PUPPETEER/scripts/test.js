ef89059b61b9288c56edab9d1db0f878b172e688
granthalf <cmonk_csec@proton.me>
2026-03-12 15:16:30 +0100

    Update test.js

--- COOKIES/PUPPETEER/scripts/test.js
+++ COOKIES/PUPPETEER/scripts/test.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const fs = require('fs');
 
 (async () => {
 	//  const browser = await puppeteer.launch();
@@ -6,16 +7,22 @@ const puppeteer = require('puppeteer');
 	//  channel: "chrome"
 	//});
 	const browser = await puppeteer.launch({
+		headless: true,
 		executablePath: "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe",
-		headless: true
+		args: ["--headless=new", "--disable-gpu"]
 	});
+	
 	console.log('Browser version:', await browser.version());
+	
 	const page = await browser.newPage();
 	await page.goto('https://www.yoururlexampledontforgettochangeithere.com');
 	console.log(await page.title());
 	
 	const cookies = await page.cookies();
+	
 	console.log(cookies);
+	fs.writeFileSync("cookies.json", JSON.stringify(cookies, null, 2));
 	
 	await browser.close();
+
 })();
